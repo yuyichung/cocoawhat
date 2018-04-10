dd4061478091b14df2a2fd85f7d21ff663380570
joeyu <joe.yu@oath.com>
2018-04-10 09:58:22 +0800

    Initial Commit

---
+++ cochawhat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cochawhat
+//
+//  Created by joeyu on 2018/4/10.
+//  Copyright © 2018 joeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cochawhat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cochawhat
+//
+//  Created by joeyu on 2018/4/10.
+//  Copyright © 2018 joeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
